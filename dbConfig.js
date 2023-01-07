a50db7ba5adf6e31cba0140199aef6434c02e9bf
Shubham Joshi <104274798+7020530266@users.noreply.github.com>
2023-01-07 22:36:44 +0530

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,5 +1,6 @@
 const mongodb = require('mongodb')
 const dbName = 'Webcode2'
-const dbUrl = `mongodb+srv://Shubham:Shubham1999@shubham.d1qasaw.mongodb.net/${dbName}?retryWrites=true&w=majority`
+const dbUrl = `mongodb+srv://Shubham:Shubham@shubham.d1qasaw.mongodb.net/${dbName}?retryWrites=true&w=majority`
+
 const MongoClient=mongodb.MongoClient
 module.exports ={mongodb,dbName,dbUrl,MongoClient}
