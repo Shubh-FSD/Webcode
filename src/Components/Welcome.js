fb3a1d3d878ddd07daec0ba065a87d6103c9100f
Shubham Joshi <104274798+7020530266@users.noreply.github.com>
2023-01-26 14:02:21 +0530

    Update Welcome.js

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -9,33 +9,14 @@ export default function Welcome() {
   //  let handleBeforeLogin = async ()=>{ navigate('/loginBefore')}
   //  let handleRegister = async ()=>{ navigate('/Register')}
   return (
-    <div id="content" className="wallpaper">
-      <Navbar1 />
-    <div >
+    <div class="container-fluid" >
+     <Navbar1 />
       <div className="Entry-Caption ">
-        <div className="row">
-          <div className="Adjust_text">
             <h1>Hi, Welcome to The INDO Group</h1>
-          </div>
-          <div className="Adjust_text">
             <p>We are the top ranked CRM service provider in India</p>
-          </div>
-          <div className="Adjust_text">
-            <img className="Capture4" src={Capture4} alt="..." />
-          </div>
+            {/* <img className="Capture4" src={Capture4} alt="..." /> */}
         </div>
-      </div>
-      <footer className="sticky-footer format">
-        <div className="container my-auto">
-          <div
-            className="copyright text-center my-auto"
-            style={{ color: "white" }}
-          >
-            <span>Copyright &copy; Your Website 2022</span>
-          </div>
         </div>
-      </footer>
-      </div>
-    </div>
   );
 }
+
